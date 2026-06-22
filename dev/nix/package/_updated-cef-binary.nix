044d04294179557e0ace4f993c79854a39226e4d
xaltsc <hey+dev@xaltsc.dev>
2026-06-22 17:28:48 +0200

    flake: add update script for cef

--- dev/nix/package/_updated-cef-binary.nix
+++ dev/nix/package/_updated-cef-binary.nix
@@ -1,5 +1,5 @@
 { cef-binary }:
-cef-binary.override {
+(cef-binary.override {
   version = "149.0.4";
   gitRevision = "2f1bfd8";
   chromiumVersion = "149.0.7827.156";
@@ -7,4 +7,7 @@ cef-binary.override {
     aarch64-linux = "sha256-iQmnlonux7I+2ACEtpdmlS1E4A+aNFgylRsykD+KgKA=";
     x86_64-linux = "sha256-bUNgdnXkfta/pA0c/OE20E53IFKfjxxENdS6Hc0YObI=";
   };
-}
+}).overrideAttrs
+  (old: {
+    passthru.updateScript = ./_update-cef.sh;
+  })
